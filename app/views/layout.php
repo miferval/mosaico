63c0e8f62147f5d6fe7d479aeea9ddea7804ffe8
Miguel Fernandez del Valle <miguelfernandezdelvalle@gmail.com>
2016-10-21 10:46:12 -0500

    Create layout.php

---
+++ app/views/layout.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* layout.php
+* version 1.0.0
+*/
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<!--STYLES-->
+	<link rel="stylesheet" href="/app/views/style.css" />
+	<!--FONTS-->
+
+</head>
+<body>
+	<div id="top_bar">
+		<div class="container">
+			<div id="logo" class="left">
+			<a href="/">
+			  <h1>logo</h1>	
+			</a>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div class="container">
+		<div id="content" class="container">
+			<!--main content-->
+			<div class="clear"></div>
+		</div>
+	</div>
+	<!--SCRIPTS-->
+	
+</body>
+</html>
